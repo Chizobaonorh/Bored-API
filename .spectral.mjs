b22c56eefe489cb828cf30cf980bc6ec7f237405
Tcheesy <onorhreward@gmail.com>
2023-10-09 06:04:22 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyMjAzNA/spectral.js?branch_slug=master&token=123f4f59-fc25-4075-a369-5d21c949d236";
+export default { extends: ruleset };
